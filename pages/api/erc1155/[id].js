ee98796f6cd03f9d69542a0345c13db1b0c13f75
fh <felixhartantio@gmail.com>
2024-03-18 15:18:21 +0700

    update cors allowance to allow all

--- pages/api/erc1155/[id].js
+++ pages/api/erc1155/[id].js
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 export default function handler(req, res) {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
