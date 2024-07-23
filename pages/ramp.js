e54abf26e6662b70b5e4c16a72c895a989cc04a1
fh <felixhartantio@gmail.com>
2024-07-23 17:59:46 +0700

    maintenance onramp

--- pages/ramp.js
+++ pages/ramp.js
@@ -2,6 +2,7 @@ import Disconnected from '../components/general/Disconnected';
 import Head from 'next/head';
 import { useAccount } from '@particle-network/connectkit';
 import RampHome from '../components/ramp/RampHome';
+import Maintenance from '../components/general/Maintenance';
 
 export default function Ramp(props) {
   const account = useAccount();
@@ -11,7 +12,8 @@ export default function Ramp(props) {
         <Head>
           <title>Bridge | Wagon Network</title>
         </Head>
-        {
+        <Maintenance/>
+        {/* {
             !account 
             ? <div className='h-full'>
                 <Disconnected {...props}/>
@@ -19,7 +21,7 @@ export default function Ramp(props) {
             : <div className='h-full'>
                 <RampHome {...props}/>
             </div>
-        }
+        } */}
     </div>
   )
 }
