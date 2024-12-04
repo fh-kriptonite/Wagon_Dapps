bd5119a269ba28b0d4e9169010ac51dbeaa4284e
fh-kriptonite <fh@kriptonite.co>
2024-12-04 17:21:30 +0700

    change sidebar toggle with usestate

--- components/general/Sidebar.js
+++ components/general/Sidebar.js
@@ -9,6 +9,7 @@ import { CgProfile } from "react-icons/cg";
 
 import Link from 'next/link'
 import { ConnectButton } from '@particle-network/connectkit';
+import { useState } from 'react';
 
 export default function Sidebar(props) {
     const router = useRouter();
@@ -17,13 +18,29 @@ export default function Sidebar(props) {
     // Access the current pathname
     const currentPath = router.pathname;
 
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    // Add `isSidebarOpen` class logic to the sidebar
+    const sidebarClass = isSidebarOpen
+        ? "translate-x-0"
+        : "-translate-x-full";
+
     return (
         <>
             <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200">
                 <div className="px-3 py-3 lg:px-5 lg:pl-3">
                     <div className="flex items-center justify-between">
                         <div className="flex items-center justify-start">
-                            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600">
+                            <button 
+                                onClick={toggleSidebar}
+                                 aria-controls="logo-sidebar" 
+                                 type="button" 
+                                 className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600"
+                            >
                                 <span className="sr-only">Open sidebar</span>
                                 <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
@@ -43,7 +60,11 @@ export default function Sidebar(props) {
                 </div>
             </nav>
 
-            <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-56 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+            <aside
+                id="logo-sidebar"
+                className={`fixed top-0 left-0 z-40 w-56 h-screen pt-20 transition-transform ${sidebarClass} bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700`}
+                aria-label="Sidebar"
+            >    
                 <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                     <ul className="space-y-2 font-medium pb-2 border-b border-gray-200 dark:border-gray-700 text-sm">
                         <li>
