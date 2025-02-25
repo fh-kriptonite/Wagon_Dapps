2d66d8054e55829e5424be4e3cc570ec10fcbaa8
fh-kriptonite <fh@kriptonite.co>
2025-02-25 14:47:09 +0700

    reskin disconnected page

--- components/general/Disconnected.js
+++ components/general/Disconnected.js
@@ -5,7 +5,11 @@ export default function Disconnected(props) {
     return (
         <div className='h-full flex'>
             <div className='m-auto text-center'>
-                <img src="/logo.png" className="h-16 mx-auto" alt="Wagon Logo" />
+                {
+                    process.env.THEME_SKIN == 2
+                    ? <img src="/logo-waresix-square.png" className="h-32 mx-auto rounded-full" alt="Logo" />
+                    : <img src="/logo.png" className="h-16 mx-auto" alt="Logo" />
+                }
                 <h5 className="text-xl font-bold mt-5">
                     Please, connect your wallet
                 </h5>
