90c2bb62c9e27dfa2547242c65c261685874d417
fh-kriptonite <fh@kriptonite.co>
2025-02-26 10:39:04 +0700

    update view lend card

--- components/lend/LendHome.js
+++ components/lend/LendHome.js
@@ -81,7 +81,7 @@ export default function LendHome(props) {
   
         {
           pools.length > 0
-          ? <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4 mb-6">
+          ? <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4 mb-6 items-end">
               {
                 pools.map((pool, index)=>{
                   return (
