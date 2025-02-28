1801eaef0726cbbc2ff81db0420febbcf5dca589
fh-kriptonite <fh@kriptonite.co>
2025-02-28 09:26:06 +0700

    bug - about borrower is blank

--- pages/lend/[poolId].js
+++ pages/lend/[poolId].js
@@ -231,7 +231,7 @@ export default function Pool() {
           <Tabs.Item title="Borrowers" icon={PiUserFill}>
               <div className='min-h-60'>
                 <AboutBorrower
-                  poolJson={poolJson}
+                  poolJson={poolJson?.properties.borrower}
                 />
               </div>
           </Tabs.Item>
